045bf64e4e7bd37144a38c3f21645fb322ecea05
Tyrica747 <tyrica747@gmail.com>
2023-09-07 02:04:36 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - Main function
+ *main - Entry point
  *
  *return: 0 (success)
  */
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - main function
+ * main - entry point
  *
  * Return: return 0
  */
