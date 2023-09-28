e48a1102eb4ec5090559a404764605a7bcab815f
Tyrica747 <tyrica747@gmail.com>
2023-09-28 06:55:50 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,12 +3,12 @@
 int actual_prime(int n, int i);
 
 /**
- * is_prime_number  says if  an integer is a prime number or not
+ * is_prime_number - says if  an integer is a prime number or not
  * @n: number to evaluate
  *
  * Return: 1 if n is a prime number, 0 if not
  */
-int is_prime_numbber(int n)
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
